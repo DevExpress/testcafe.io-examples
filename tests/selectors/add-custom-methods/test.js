b525500013e5f26bda489e0c8d589e2cec384492
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-10-26 12:30:57 +0400

    test: added Selector.addCustomMethods

---
+++ tests/selectors/add-custom-methods/test.js
@@ -0,0 +1,30 @@
+import { Selector } from 'testcafe';
+
+fixture`Selector.addCustomMethods`
+    .page`https://js.devexpress.com/`;
+
+test( 'Check table', async t => {
+    const myTable = Selector( '.dx-datagrid-table' )
+        .nth( 1 )
+        .addCustomMethods( {
+            getExpandButtonCell: ( elements, rowIndex ) => {
+                return elements[0].querySelectorAll( '.dx-group-row' )[rowIndex].cells[0];
+            },
+            // ...
+            // Other methods that return DOM nodes.
+        }, {
+            returnDOMNodes: true,
+        } )
+        .addCustomMethods( {
+            getCellText: ( table, rowIndex, columnIndex ) => {
+                return table.rows[rowIndex].cells[columnIndex].innerText;
+            },
+            // ...
+            // Other methods that return serializable objects.
+        } );
+
+    await t
+        .expect( myTable.getCellText( 3, 1 ) ).contains( 'Europe' )
+        .click( myTable.getExpandButtonCell( 0 ) )
+        .expect( myTable.getCellText( 1, 1 ) ).contains( 'North America' );
+} );
