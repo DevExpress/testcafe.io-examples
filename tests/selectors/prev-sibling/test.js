8ab92e2687a806c99d6ae62b1564c67b7b024972
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-10-26 17:05:43 +0400

    test: added test Selector.prevSibling

---
+++ tests/selectors/prev-sibling/test.js
@@ -0,0 +1,24 @@
+import { Selector } from 'testcafe';
+
+fixture`Selector.prevSibling`
+    .page`https://devexpress.github.io/testcafe/example/`;
+
+test('Check count of the filtered elements', async (t) => {
+    // eslint-disable-next-line no-unused-vars
+    Selector('section').prevSibling((node, idx, originNode) => {
+        // node === the <section>'s preceding sibling node
+        // idx === index of the current <section>'s preceding sibling node
+        // originNode === the <section> element
+    });
+
+    const isNodeOk = (node, idx, originNode) => {
+        console.log({ node, idx, originNode });
+        return idx === 6;
+    };
+
+    const filteredDiv = Selector('div').prevSibling((node) => {
+        return !isNaN(node.textContent);
+    }, { isNodeOk });
+
+    await t.expect(filteredDiv.count).eql(10);
+});
