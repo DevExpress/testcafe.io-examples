0cea4af72e0e8b3b0d914062b008538ed7e10a6f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-10-28 11:57:06 +0400

    test: fixed comment in Selector.filterHidden

--- tests/selectors/filterHidden/test.js
+++ tests/selectors/filterHidden/test.js
@@ -4,7 +4,7 @@ fixture`Selector.filterHidden`
     .page`http://devexpress.github.io/testcafe/example/`;
 
 test('Check count of the hidden divs', async (t) => {
-    // Selects all hidden label elements.
+    // Selects all hidden div elements.
     const hiddenSelectors = Selector('div').filterHidden();
 
     await t.expect(hiddenSelectors.count).eql(2);
