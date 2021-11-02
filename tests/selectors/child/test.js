59cacbf1e5c7623284f194d5ae42c85b6a4a48b5
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-02 12:47:04 +0400

    test: refactored according the doc

--- tests/selectors/child/test.js
+++ tests/selectors/child/test.js
@@ -7,20 +7,17 @@ test('Check count of the elements children', async (t) => {
     // Selects all children of all fieldset elements.
     const fieldsetChildren = Selector('fieldset').child();
 
-    await t.expect(fieldsetChildren.count).eql(20);
-
     // Selects all closest children of all div elements.
     const divClosestChildren = Selector('div').child(0);
-
-    await t.expect(divClosestChildren.count).eql(8);
 
     // Selects all furthest children of all .column elements.
     const columnFurthestChildren = Selector('.column').child(-1);
 
-    await t.expect(columnFurthestChildren.count).eql(2);
-
     // Selects all p elements that are children of a fieldset element.
     const fieldsetPChildren = Selector('fieldset').child('p');
 
+    await t.expect(fieldsetChildren.count).eql(20);
+    await t.expect(divClosestChildren.count).eql(8);
+    await t.expect(columnFurthestChildren.count).eql(2);
     await t.expect(fieldsetPChildren.count).eql(8);
 });
