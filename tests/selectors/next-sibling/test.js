88c5980fb8a5b87419d74523a532e1f9e470256c
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-03 12:12:14 +0400

    test: fixed order of commands in the Check count of the succeeding sibling elements.

--- tests/selectors/next-sibling/test.js
+++ tests/selectors/next-sibling/test.js
@@ -7,20 +7,17 @@ test('Check count of the succeeding sibling elements.', async (t) => {
     // Selects all succeeding siblings of all 'header' elements.
     const siblingsHeader = Selector('header').nextSibling();
 
-    await t.expect(siblingsHeader.count).eql(3);
-
     // Selects all closest succeeding siblings of all div elements.
     const closestSiblingsDiv = Selector('div').nextSibling(0);
-
-    await t.expect(closestSiblingsDiv.count).eql(13);
 
     // Selects all furthest succeeding siblings of all fieldset elements.
     const furthestSiblingsFieldset = Selector('fieldset').nextSibling(-1);
 
-    await t.expect(furthestSiblingsFieldset.count).eql(3);
-
     // Selects all p elements that are succeeding siblings of an legend element.
     const pSiblingsDiv = Selector('legend').nextSibling('p');
 
+    await t.expect(siblingsHeader.count).eql(3);
+    await t.expect(closestSiblingsDiv.count).eql(13);
+    await t.expect(furthestSiblingsFieldset.count).eql(3);
     await t.expect(pSiblingsDiv.count).eql(8);
 });
