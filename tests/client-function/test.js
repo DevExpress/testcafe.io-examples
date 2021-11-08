bd3241763882a27b879a14f3965bf461291344fd
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-08 12:18:28 +0400

    test: added test ClientFunction

---
+++ tests/client-function/test.js
@@ -0,0 +1,12 @@
+import { ClientFunction } from 'testcafe';
+
+const getWindowLocation = ClientFunction(() => window.location);
+
+fixture`ClientFunction`
+    .page`https://devexpress.github.io/testcafe/example/`;
+
+test('Check location', async t => {
+    const location = await getWindowLocation();
+
+    await t.expect(location.href).eql('https://devexpress.github.io/testcafe/example/');
+});
