4f9ea778dbc61127b4440db6dc8ce87aa3e893ae
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-10 13:06:01 +0400

    test: added test Request filter - exact url

---
+++ tests/request/request-filter/exact-url.test.js
@@ -0,0 +1,17 @@
+import { RequestLogger, RequestMock } from 'testcafe';
+
+const logger = RequestLogger(['https://devexpress.github.io/testcafe/example/', 'http://localhost:8080']);
+
+const mock = RequestMock()
+    .onRequestTo('http://external-service.com/api/')
+    .respond('Mocked respond');
+
+fixture`Request filter - exact url`
+    .page`https://devexpress.github.io/testcafe/example/`
+    .requestHooks([logger, mock]);
+
+test('The external request shouldn\'t be logged', async t => {
+    await t.expect(logger.requests.length).eql(1)
+        .navigateTo('http://external-service.com/api/')
+        .expect(logger.requests.length).eql(1);
+});
