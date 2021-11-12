74a98fa6ca86bf457228645e676bb181397f1320
Popov Aleksey <Alekseypopow1995@yandex.ru>
2021-11-12 13:16:12 +0400

    test: added test RequestMock.constructor

---
+++ tests/request/request-mock/constructor/test.js
@@ -0,0 +1,14 @@
+import { RequestMock, Selector } from 'testcafe';
+
+const mock = RequestMock()
+    .onRequestTo(/google.com/)
+    .respond('', 404);
+
+fixture`RequestMock.constructor`
+    .page`google.com`
+    .requestHooks(mock);
+
+test('Should mock requests', async t => {
+    await t
+        .expect(Selector('body').child().count).eql(0);
+});
