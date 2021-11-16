58e35f72705d272a139a46b4736350f8593f3e4f
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-11-16 11:16:41 +0300

    test: added event-options.js

--- tests/request/request-hook/class/event-options.js
+++ tests/request/request-hook/class/event-options.js
@@ -1,23 +1,10 @@
 /* eslint-disable no-unused-vars,constructor-super */
 import { RequestHook } from 'testcafe';
 
-class JwtBearerAuthorization extends RequestHook {
-    constructor () {
-        super();
-    }
-
-    onRequest (e) {
-        e.requestOptions.headers['Authorization'] = 'generate token here';
-    }
-
-    onResponse (e) {
-    }
-}
-
 class MyFirstHook extends RequestHook {
     constructor (requestFilterRules, responseEventOptions) {
-        console.log(requestFilterRules[0]); // https://example.com
-        console.log(requestFilterRules[1]); // /auth.mycorp.com/
+        console.log(responseEventOptions.includeHeaders); // false
+        console.log(responseEventOptions.includeBody); // false
     }
 }
 
