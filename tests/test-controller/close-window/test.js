6fffbf2e2bf35ed4225a1b5d7de56f7f83f9cb9f
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-09 11:18:09 +0300

    test: added test TestController.closeWindow -> Closing windows

---
+++ tests/test-controller/close-window/test.js
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe';
+
+fixture`TestController.closeWindow`
+    .page`https://devexpress.github.io/testcafe/example/`;
+
+test('Closing windows', async t => {
+    await t.openWindow('https://devexpress.github.io/testcafe')
+        .openWindow('https://devexpress.com')
+        .closeWindow()
+        .closeWindow();
+});
