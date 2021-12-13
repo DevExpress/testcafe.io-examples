be26b94e8716cbe9cc3119f3e8da123d4e404c5a
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-13 09:56:25 +0300

    test: added test Action speed

---
+++ tests/templates/actions/basic-action-options.ts
@@ -0,0 +1,16 @@
+interface basicOption {
+    speed: Number;
+}
+
+import { Selector } from 'testcafe';
+
+const nameInput = Selector('#developer-name');
+
+fixture`Action speed`
+    .page`https://devexpress.github.io/testcafe/example/`;
+
+test('Type name', async t => {
+    await t
+        .typeText(nameInput, 'Peter')
+        .typeText(nameInput, ' Parker', { speed: 0.1 });
+});
