12b5a6ae38f2d6da51e8ec95bcf216f856db4421
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-14 12:02:26 +0300

    test: added test Assertion timeout

---
+++ tests/templates/assertions/timeout.test.js
@@ -0,0 +1,18 @@
+import { Selector } from 'testcafe';
+
+fixture`Assertion timeout`
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Should wait for a value of an element in a timeout', async (t) => {
+    const startTime    = Date.now();
+    const timeoutValue = 500;
+
+    try {
+        await t.expect(Selector('h1').innerText).eql('text', 'check element text', { timeout: timeoutValue });
+    }
+    catch (e) {
+        const tookTime = Date.now() - startTime;
+
+        await t.expect(tookTime > timeoutValue && tookTime < timeoutValue * 2).ok();
+    }
+});
