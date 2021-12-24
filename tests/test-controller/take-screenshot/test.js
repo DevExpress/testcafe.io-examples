57f9e761084dfda1e66698ca960d48ea4776f6cc
Aleksey Popov <alekseypopow1995@yandex.ru>
2021-12-24 12:33:20 +0300

    test: added name of the test TestController.takeScreenshot

---
+++ tests/test-controller/take-screenshot/test.js
@@ -0,0 +1,12 @@
+fixture`TestController.takeScreenshot`
+    .page`https://devexpress.github.io/testcafe/example/`;
+
+test('Take a screenshot of a fieldset', async t => {
+    await t
+        .typeText('#developer-name', 'Peter Parker')
+        .click('#submit-button')
+        .takeScreenshot({
+            path:     'my-fixture/thank-you-page.png',
+            fullPage: true,
+        });
+});
