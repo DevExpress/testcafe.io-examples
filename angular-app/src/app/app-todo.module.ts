e2a5cab6a494a7f680dfc7d4bd92e9320392c790
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-02-12 12:50:30 +0300

    refactor: deleted tests for frameworks

--- angular-app/src/app/app-todo.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TodoModule } from '../todo/todo.module';
-
-@NgModule({
-  imports: [TodoModule],
-  exports: [TodoModule]
-})
-export class AppTodoModule { }
--- tests/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const REACT_PORT = 3001;
-export const ANGULAR_PORT = 3002;
