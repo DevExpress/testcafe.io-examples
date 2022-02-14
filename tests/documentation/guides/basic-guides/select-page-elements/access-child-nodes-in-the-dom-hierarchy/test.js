4906e1784301683ed216dff7554c28677a151588
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-02-14 11:33:40 +0300

    test: added test Access Child Nodes in the DOM Hierarchy - Check the text content

---
+++ tests/documentation/guides/basic-guides/select-page-elements/access-child-nodes-in-the-dom-hierarchy/test.js
@@ -0,0 +1,16 @@
+import { Selector } from 'testcafe';
+
+fixture`Access Child Nodes in the DOM Hierarchy`
+    .page`../pages/adjacent-elements.html`;
+
+const body              = Selector('body');
+const firstChildElement = body.child(0); // <p>
+const firstChildNode    = body.find((node, index) => { // text node
+    return index === 0;
+});
+
+test('Check the text content', async t => {
+    await t
+        .expect(firstChildElement.textContent).eql('My first paragraph.')
+        .expect(firstChildNode.textContent).eql('\n    This is my tested page. ');
+});
