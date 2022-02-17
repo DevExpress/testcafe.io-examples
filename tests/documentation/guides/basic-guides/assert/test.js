9a9dd312fff9f2bf123f1f59e54d2f523013a420
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-02-17 09:40:38 +0300

    test: added test Smart Assertion Query Mechanism - Button click

--- tests/documentation/guides/basic-guides/assert/test.js
+++ tests/documentation/guides/basic-guides/assert/test.js
@@ -11,3 +11,17 @@ test('Check property of element', async t => {
         .typeText(developerNameInput, 'Peter Parker')
         .expect(developerNameInput.value).contains('Peter', 'input contains text "Peter"');
 });
+
+fixture`Smart Assertion Query Mechanism`
+    .page`../pages/index.html`;
+
+test('Button click', async t => {
+    const btn = Selector('#btn');
+
+    await t
+        .click(btn)
+        // A regular assertion fails immediately, but TestCafe retries to run DOM state
+        // assertions many times within the timeout until this assertion passes successfully.
+        // The default timeout is 3000 ms.
+        .expect(btn.textContent).contains('Loading...');
+});
