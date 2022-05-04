c6c08298dae76ff5536936ca3709b1db9082a43e
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-05-04 17:07:57 +0400

    test: added tests for cookie api

--- tests/documentation/reference/test-api/test-controller/delete-cookies/test.js
+++ tests/documentation/reference/test-api/test-controller/delete-cookies/test.js
@@ -1,11 +1,65 @@
 fixture`[API] Delete Cookies`
-    .page('https://devexpress.github.io/testcafe/example/')
-    .beforeEach(async t => {
-        await t.setCookies({ name: 'apiCookie1', value: 'value1' });
-    });
+    .page('https://devexpress.github.io/testcafe/example/');
 
 test('Should delete a cookie with the specified name', async t => {
+    await t.setCookies({ name: 'apiCookie1', value: 'value1' });
     await t.expect((await t.getCookies()).length).eql(1);
-    await t.deleteCookies('apiCookie1', 'https://devexpress.github.io/testcafe/example/');
+
+    await t.deleteCookies('apiCookie1');
     await t.expect((await t.getCookies()).length).eql(0);
+});
+
+fixture('[API] Delete Cookies')
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Should delete cookies for a specified url', async t => {
+    //set a cookie for example's page
+    await t.setCookies({ name: 'apiCookie1', value: 'value1' });
+
+    //set a cookie for localhost page
+    await t.setCookies({ name: 'apiCookie2', value: 'value2' }, 'http://localhost');
+
+    //check all tested page cookies
+    await t.expect((await t.getCookies()).length).eql(2);
+
+    //delete localhost page's cookie
+    await t.deleteCookies('apiCookie2', 'http://localhost');
+
+    const cookies = await t.getCookies();
+
+    await t
+        .expect(cookies.length).eql(1)
+        .expect(cookies[0]).contains({ name: 'apiCookie1', value: 'value1', domain: 'devexpress.github.io' });
+});
+
+fixture('[API] Delete Cookies')
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Should delete all cookies for specified url', async t => {
+    //set a cookie for example's page
+    await t.setCookies({ name: 'apiCookie1', value: 'value1' });
+
+    //set a cookie for thank-you's page
+    await t.setCookies({
+        name:  'apiCookie2',
+        value: 'value2',
+    }, 'https://devexpress.github.io/testcafe/example/thank-you.html');
+
+    //check cookies
+    let cookies = await t.getCookies();
+
+    await t
+        .expect(cookies.length).eql(2)
+        .expect(cookies[0]).contains({ name: 'apiCookie1', path: '/testcafe/example/' })
+        .expect(cookies[1]).contains({ name: 'apiCookie2', path: '/testcafe/example/thank-you.html' });
+
+    //delete cookies from thank-you page
+    await t.deleteCookies({ domain: 'devexpress.github.io', path: '/testcafe/example/thank-you.html' });
+
+    //check cookies
+    cookies = await t.getCookies();
+
+    await t
+        .expect(cookies.length).eql(1)
+        .expect(cookies[0]).contains({ name: 'apiCookie1', path: '/testcafe/example/' });
 });
--- tests/documentation/reference/test-api/test-controller/get-cookies/test.js
+++ tests/documentation/reference/test-api/test-controller/get-cookies/test.js
@@ -12,3 +12,59 @@ test('Should get cookies by name', async t => {
         .expect(name).eql('apiCookie1')
         .expect(value).eql('value1');
 });
+
+fixture('[API] Get Cookies')
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Should get cookies for a specified url', async t => {
+    //set a cookie for example's page
+    await t.setCookies({ name: 'apiCookie1', value: 'value1' });
+
+    //set a cookie for localhost page
+    await t.setCookies({ name: 'apiCookie2', value: 'value2' }, 'http://localhost');
+
+    //get all tested pages' cookies
+    let cookies = await t.getCookies(['apiCookie1', 'apiCookie2']);
+
+    await t.expect(cookies.length).eql(2);
+
+    //get example page's cookies
+    cookies = await t.getCookies(['apiCookie1', 'apiCookie2'], 'https://devexpress.github.io/testcafe/example/');
+    await t.expect(cookies.length).eql(1);
+
+    //get localhost page's cookies
+    cookies = await t.getCookies(['apiCookie1', 'apiCookie2'], 'https://localhost');
+    await t.expect(cookies.length).eql(1);
+});
+
+fixture('[API] Get Cookies')
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Should get all cookies for specified url', async t => {
+    //set a cookie for example's page
+    await t.setCookies({ name: 'apiCookie1', value: 'value1' });
+
+    //set a cookie for thank-you's page
+    await t.setCookies({
+        name:  'apiCookie2',
+        value: 'value2',
+    }, 'https://devexpress.github.io/testcafe/example/thank-you.html');
+
+    //get all cookies for devexpress.github.io
+    let cookies = await t.getCookies({ domain: 'devexpress.github.io' });
+
+    await t.expect(cookies.length).eql(2);
+
+    //get all cookies for example's page
+    cookies = await t.getCookies({ domain: 'devexpress.github.io', path: '/testcafe/example/' });
+    await t
+        .expect(cookies.length).eql(1)
+        .expect(cookies[0]).contains({ name: 'apiCookie1', value: 'value1' });
+
+    //get all cookies for thank-you's page
+    cookies = await t.getCookies({ domain: 'devexpress.github.io', path: '/testcafe/example/thank-you.html' });
+    await t
+        .expect(cookies.length).eql(2)
+        .expect(cookies[0]).contains({ name: 'apiCookie1', value: 'value1' })
+        .expect(cookies[1]).contains({ name: 'apiCookie2', value: 'value2' });
+});
