7b3ab2d981495f49b262a4bff0653ae8adeb9cec
titerman <43554315+titerman@users.noreply.github.com>
2023-05-29 16:20:50 +0400

    Update the getting started test

--- tests/documentation/getting-started/test.js
+++ tests/documentation/getting-started/test.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-unused-vars */
 import { Selector } from 'testcafe';
 
-fixture`Getting Started`
-    .page`https://devexpress.github.io/testcafe/example`;
+fixture('Getting Started')
+    .page('https://devexpress.github.io/testcafe/example');
 
 test('My first test', async t => {
-    // Test code
+    await t
+        .typeText('#developer-name', 'John Smith')
+        .click('#submit-button')
+        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
 });
